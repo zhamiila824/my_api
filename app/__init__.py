a7c47278db4ef26214e902bd3c031eb31c6bbc23
zhamiila824 <jama.98.kjk@gmail.com>
2022-09-14 23:45:26 +0600

    init of app & const and models of users

--- app/__init__.py
+++ app/__init__.py
@@ -42,7 +42,3 @@ if not app.config['DEBUG']:
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
-
-
-from app.users.views import mod as usersModule
-app.register_blueprint(usersModule)
---
+++ app/users/models.py
@@ -0,0 +1,27 @@
+from app import db
+from app.users import constants as USER
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    role = db.Column(db.SmallInteger, default=USER.USER)
+    status = db.Column(db.SmallInteger, default=USER.NEW)
+
+    def __int__(self, username=None, email=None, password=None):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def getStatus(self):
+        return USER.STATUS[self.status]
+
+    def getRole(self):
+        return USER.ROLE[self.role]
+
+    def __repr__(self):
+        return '<User %r>' % self.username
